bd94b9be2cea3e5545ac31ac2bca7145daf87785
Alexander Vasilchenko <sanloid2@gmail.com>
2023-02-08 00:25:17 +0600

    vercel crosorigin

--- src/config/environments.ts
+++ src/config/environments.ts
@@ -2,6 +2,6 @@
 
 export default {
   apiKey: 'ee3b29bd727d842603cb54ddc7fcbce8',
-  baseApiUrl: 'http://gateway.marvel.com/v1/public/',
+  baseApiUrl: 'https://gateway.marvel.com/v1/public/',
   privateKey: 'd042c382a2a67aa31fdae7da6cc7ddf12811e0ad'
 };
